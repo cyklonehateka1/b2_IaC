5882ed8bb6afb814375b06f0fa72d1a795f7d4a3
cyklonehateka1 <emmanuelhateka1@gmail.com>
2025-12-11 01:13:04 +0000

    Fixed AMI type issue

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "instance_public_ip" {
-  value       = aws_instance.web_server.public_ip
+  value       = aws_instance.web.public_ip
   description = "Public IP address of the EC2 instance"
 }
